9d96a7bdc04160da7f82bbcd0528a9e150e70a2a
Meir Taffel <artgardening@gmail.com>
2019-11-12 11:56:21 -0500

    Remove Radium. Add styled-components for css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: green;
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: lightgreen;
+    color : black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -82,18 +97,16 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button
-            style={style}
-            onClick={this.togglePersonsHandler}>Toggle Persons</button>
-          {persons}
-        </div>
-      </StyleRoot>
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <StyledButton onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </StyledButton>
+        {persons}
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
