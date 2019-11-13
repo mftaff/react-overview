02cfe62d047a14784abd622292883e0bd10c7207
Meir Taffel <artgardening@gmail.com>
2019-11-13 12:21:59 -0500

    Add useEffect to functional component example

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import classes from './Cockpit.css';
 
 const cockpit = props => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    // HTTP requests from here = OK
+  });
+
   const assignedClasses = [];
   let btnClass = '';
   if (props.showPersons) {
