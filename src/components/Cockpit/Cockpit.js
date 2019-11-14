0a693a1df5390ec83a3e5910e4ed0c6815f58dca
Meir Taffel <artgardening@gmail.com>
2019-11-14 13:31:03 -0500

    Demonstrate refs in functional components (useRef + useEffect)

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,19 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import classes from './Cockpit.css';
 
 const cockpit = props => {
+  const toggleBtnRef = useRef(null);
+
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
-    // HTTP requests from here = OK
-    setTimeout(() => {
-      alert('Saved data to cloud.');
-    }, 1000);
-    return () => {
-      console.log('[Cockpit.js] use to do cleanup work on functional component');
-    };
-  // }, [props.persons]); // use to limit it to when this data is touched
-  }, []); // use for on create only
+    toggleBtnRef.current.click();
+  }, []);
+
+  // useEffect(() => {
+  //   console.log('[Cockpit.js] useEffect');
+  //   // HTTP requests from here = OK
+  //   setTimeout(() => {
+  //     alert('Saved data to cloud.');
+  //   }, 1000);
+  //   return () => {
+  //     console.log('[Cockpit.js] use to do cleanup work on functional component');
+  //   };
+  // // }, [props.persons]); // use to limit it to when this data is touched
+  // }, []); // use for on create only
 
   // useEffect()
 
@@ -34,7 +41,7 @@ const cockpit = props => {
     <div className={classes.Cockpit}>
       <h1>{props.title}</h1>
       <p className={assignedClasses.join(' ')}>This is really working!</p>
-      <button className={btnClass} onClick={props.clicked}>
+      <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle Persons
       </button>
     </div>
