618d59fa7e0b636308f41a41b0d985cac3a48907
Meir Taffel <artgardening@gmail.com>
2019-11-14 12:12:47 -0500

    Pass dynamic props through HOCs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 
 import classes from './Person.css';
+import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...')
     return (
       <React.Fragment>
-        {/* <div className={classes.Person}> */}
-          <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-          <p>{this.props.children}</p>
-          <input type="text" onChange={this.props.changed} value={this.props.name}/>
-        {/* </div> */}
+        <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
       </React.Fragment>
     )
   }
 };
 
-export default Person;
+export default withClass(Person, classes.Person);
