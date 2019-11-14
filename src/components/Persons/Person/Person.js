e587ff012b979a5711113447581800fc7a987638
Meir Taffel <artgardening@gmail.com>
2019-11-14 13:22:52 -0500

    Demonstrate refs for doing JS things to created elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,13 +5,28 @@ import classes from './Person.css';
 import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // this.inputEl.focus();
+    this.inputElRef.current.focus();
+  }
+
   render() {
     console.log('[Person.js] rendering...')
     return (
       <React.Fragment>
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
+        <input
+          ref={this.inputElRef}
+          // ref={(el) => { this.inputEl = el }}
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name} />
       </React.Fragment>
     )
   }
